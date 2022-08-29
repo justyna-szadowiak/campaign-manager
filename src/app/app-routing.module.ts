9fee6a87e5bb4b3d401ac5b9802f03d4b8f3c688
Justyna Szadowiak <justyna.elzbieta.szadowiak@gmail.com>
2022-08-29 21:45:21 +0200

    Routing and module for the app has been cofigured

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CampaignListComponent } from './campaign-list/campaign-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'app', pathMatch: 'full'},
+    {path: 'app', component: CampaignListComponent},
+  {
+    path: '**',
+    redirectTo: '/404',
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,57 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule  } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CampaignListComponent } from './campaign-list/campaign-list.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { CreateEditCampaignComponent } from './create-edit-campaign/create-edit-campaign.component';
+
+import { HttpClientModule } from '@angular/common/http';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
+import {MatListModule} from '@angular/material/list';
+import {MatDialogModule} from '@angular/material/dialog';
+import { MatChipsModule } from '@angular/material/chips';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatSelectModule} from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from "@angular/material/form-field";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CampaignListComponent,
+    NotFoundComponent,
+    CreateEditCampaignComponent,
   ],
   imports: [
+    FormsModule,
+    CommonModule,
+    ReactiveFormsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatButtonModule,
+    MatCardModule,
+    MatIconModule,
+    MatListModule,
+    MatDialogModule,
+    MatChipsModule,
+    MatAutocompleteModule,
+    MatRadioModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
