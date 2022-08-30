cbea85e4f6f0665a1d37e7fc7a6c93c4a8c2372e
Justyna Szadowiak <justyna.elzbieta.szadowiak@gmail.com>
2022-08-30 17:49:02 +0200

    FormControl in create/edit component completed

--- src/app/create-edit-campaign/create-edit-campaign.component.ts
+++ src/app/create-edit-campaign/create-edit-campaign.component.ts
@@ -2,15 +2,14 @@ import {COMMA, ENTER} from '@angular/cdk/keycodes';
 import { Component, OnInit, ElementRef, ViewChild, Inject } from '@angular/core';
 import { FormControl, FormGroup, FormBuilder } from '@angular/forms';
 import { ErrorStateMatcher } from '@angular/material/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { first, Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
 import {map, switchMap, startWith, tap} from 'rxjs/operators';
 import {MatAutocompleteSelectedEvent} from '@angular/material/autocomplete';
 import {MatChipInputEvent} from '@angular/material/chips';
 import { Campaign } from '../interfaces';
 import { AlertsService } from '../service/alerts.service';
 import { ApiService } from '../service/api.service';
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-create-campaign',
@@ -19,11 +18,17 @@ import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 })
 export class CreateEditCampaignComponent implements OnInit {
   public campaign$!: Observable<Campaign>;
-  public campaignForm!: FormGroup;
+  public campaignForm: FormGroup = this.formBuilder.group({
+    name: new FormControl(''),
+    keyword: new FormControl(''),
+    bid: new FormControl(0.5),
+    fund: new FormControl(''),
+    status: new FormControl(),
+    town: new FormControl(),
+    radius: new FormControl()
+  });;
   public towns$: Observable<string[]>;
-  public radius: number | undefined;
   public status: boolean = true;
-  public campaign!: Campaign;
   public submitted: boolean | undefined;
   public isAddMode!: boolean;
   public loading: boolean | undefined;
@@ -37,41 +42,28 @@ export class CreateEditCampaignComponent implements OnInit {
   keywordInput!: ElementRef<HTMLInputElement>;
 
   constructor(
-    @Inject(MAT_DIALOG_DATA) public id: number,
+    public dialogRef: MatDialogRef<CreateEditCampaignComponent>,
+    @Inject(MAT_DIALOG_DATA) campaign: Campaign,
     public formBuilder: FormBuilder,
     public API: ApiService,
-    public AlertAPI: AlertsService,
-    private router: Router,
-    private route: ActivatedRoute) {
-      const allKeywords$ = this.API.getKeywords();
+    public AlertAPI: AlertsService) {
+      this.campaignForm.patchValue(campaign);
+      this.isAddMode = !campaign;
       this.towns$ = this.API.getTowns();
-      this.campaign$ = this.API.getCampaignById(id);
       this.filteredKeywords$ = this.keyword.valueChanges.pipe(
         startWith(null),
         tap((userKeyword) => {this.userKeyword = userKeyword}),
-        switchMap(() => allKeywords$),
+        switchMap(() => this.API.getKeywords()),
         map((allKeywords: string[]) => this._filter(allKeywords)),
       );
     }
 
   ngOnInit(): void {
-    // this.campaign?.id = this.route.snapshot.params['id'];
-    this.isAddMode = !this.campaign?.id
-
-    this.campaignForm = this.formBuilder.group({
-      name: new FormControl(''),
-      keyword: new FormControl(''),
-      bid: new FormControl(0.5),
-      fund: new FormControl(''),
-      status: new FormControl(),
-      town: new FormControl(),
-      radius: new FormControl()
-    });
   }
 
   titleMatch = new ErrorStateMatcher()
 
-  add(event: MatChipInputEvent): void {
+  addChip(event: MatChipInputEvent): void {
     const value = (event.value || '').trim();
 
     if(value) {
@@ -79,11 +71,10 @@ export class CreateEditCampaignComponent implements OnInit {
     }
 
     event.chipInput!.clear();
-
     this.keyword.setValue(null);
   }
 
-  remove(keyword: string): void {
+  removeChip(keyword: string): void {
     const index = this.keywords.indexOf(keyword);
 
     if (index >= 0) {
@@ -91,7 +82,7 @@ export class CreateEditCampaignComponent implements OnInit {
     }
   }
 
-  selected(event: MatAutocompleteSelectedEvent): void {
+  selectedKeyword(event: MatAutocompleteSelectedEvent): void {
     this.keywords.push(event.option.viewValue);
     this.keywordInput.nativeElement.value = '';
     this.keyword.setValue(null);
@@ -113,32 +104,6 @@ export class CreateEditCampaignComponent implements OnInit {
       return;
     }
 
-    // this.loading = true;
-    // if (this.isAddMode) {
-    //   this.createCampaign();
-    // } else {
-    //   this.editCampaign();
-    // }
+    this.dialogRef.close();
   }
-
-  // createCampaign() {
-  //   this.API.saveCampaign(this.campaignForm.value)
-  //     .pipe(first())
-  //     .subscribe(() => {
-  //       this.AlertAPI.success('New campaign has been added', { keepAfterRouteChange: true});
-  //       this.router.navigate(['/app'], { relativeTo: this.route});
-  //     })
-  //     .add(() => this.loading = false);
-  // }
-
-  // editCampaign() {
-  //   const id: number = this.campaign?.id
-  //   this.API.updateCampaign(this.campaign?.id, this.campaignForm.value)
-  //     .pipe(first())
-  //     .subscribe(() => {
-  //       this.AlertAPI.success('The campaign has been updated', {keepAfterRouteChange: true});
-  //       this.router.navigate(['/app'], { relativeTo: this.route});
-  //     })
-  //     .add(() => this.loading = false);
-  // }
 }
